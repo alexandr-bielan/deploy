ab27a6b4b6baf6bb91751b09a9ea060c8fc4495a
Oleksandr Bielan <46462953+freddik@users.noreply.github.com>
2021-02-28 23:16:30 +0200

    Delete src/main/java/com/web/server/webserver/dao directory

--- src/main/java/com/web/server/webserver/dao/GameDAO.java
+++
@@ -1,7 +0,0 @@
-package com.web.server.webserver.dao;
-
-import com.web.server.webserver.domain.Game;
-
-public interface GameDAO extends CrudDAO<Game> {
-    Game findById(int id);
-}
--- src/main/java/com/web/server/webserver/dao/TournamentDAO.java
+++
@@ -1,7 +0,0 @@
-package com.web.server.webserver.dao;
-
-import com.web.server.webserver.domain.Tournament;
-
-public interface TournamentDAO extends CrudDAO<Tournament> {
-    Tournament findById(int id);
-}
