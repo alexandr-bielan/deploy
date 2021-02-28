c3db316f98e61293d903ed2e32fd5107686e30a0
Oleksandr Bielan <46462953+freddik@users.noreply.github.com>
2021-02-28 23:16:06 +0200

    Delete src/main/java/com/web/server/webserver/service directory

--- src/main/java/com/web/server/webserver/service/GameService.java
+++
@@ -1,7 +0,0 @@
-package com.web.server.webserver.service;
-
-import com.web.server.webserver.domain.Game;
-
-public interface GameService extends CRUDService<Game> {
-    Game findById(int id);
-}
--- src/main/java/com/web/server/webserver/service/ParticipantService.java
+++
@@ -1,8 +0,0 @@
-package com.web.server.webserver.service;
-
-import com.web.server.webserver.domain.Participant;
-
-public interface ParticipantService extends CRUDService<Participant>{
-    Participant findById(int id);
-
-}
