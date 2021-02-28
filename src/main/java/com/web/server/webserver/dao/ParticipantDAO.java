de0ac8239cb286a6075ab9d4385543101ba0d2b0
Oleksandr Bielan <46462953+freddik@users.noreply.github.com>
2021-02-28 23:11:40 +0200

    Delete src directory

--- src/main/java/com/web/server/webserver/dao/ParticipantDAO.java
+++
@@ -1,8 +0,0 @@
-package com.web.server.webserver.dao;
-
-import com.web.server.webserver.domain.Participant;
-
-public interface ParticipantDAO extends CrudDAO<Participant> {
-    Participant findById(int id);
-
-}
